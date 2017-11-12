ad3478b26970ced441fbc3cc56403af51b69a004
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 12:56:13 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,19 @@
 'use strict';
+class Question{
+  constructor(content){
+    this.content = content;
+    this.save();
+
+  }
+static All(){
+  return this._All;
+  }
+  save(){
+    this.constructor._All.push(this)
+  }
+  static Find(id){
+    return this.All()[id - 1]
+  }
+}
+
+Question._All = []
