9897c464c6c76f9d6b9100f79a7982582ac3a8ba
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 12:24:37 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,17 +1,4 @@
 'use strict';
 class Question{
-  constructor(){
-    
-  }
 
-  Question._All =[]
-  static All{
-    return this._All;
-  }
-  save(){
-    this.constructor._All.push(this)
-  }
-  static Find(id){
-    return this.constructor._all[id - 1]
-  }
 }
